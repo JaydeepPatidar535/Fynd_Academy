90df0d165f4ee13d9aec8ab1f2acfb68b80bf572
JaydeepPatidar535 <jaydeeppatidar535@gmail.com>
2023-10-04 17:35:48 +0530

    New files

---
+++ JavaScript/loops.js
@@ -0,0 +1,21 @@
+// For Loop
+console.log("For Loop:");
+for (let i = 0; i < 5; i++) {
+  console.log("Iteration " + i);
+}
+
+// While Loop
+console.log("\nWhile Loop:");
+let j = 0;
+while (j < 5) {
+  console.log("Iteration " + j);
+  j++;
+}
+
+// Do...While Loop
+console.log("\nDo...While Loop:");
+let k = 0;
+do {
+  console.log("Iteration " + k);
+  k++;
+} while (k < 5);
---
+++ JavaScript/operators.js
@@ -0,0 +1,56 @@
+// Arithmetic Operators
+let x = 10;
+let y = 5;
+let addition = x + y;
+let subtraction = x - y;
+let multiplication = x * y;
+let division = x / y;
+let modulus = x % y;
+
+console.log("Arithmetic Operators:");
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+console.log("Modulus:", modulus);
+
+// Assignment Operators
+let value = 10;
+value += 5; // Equivalent to: value = value + 5;
+value -= 3; // Equivalent to: value = value - 3;
+value *= 2; // Equivalent to: value = value * 2;
+value /= 4; // Equivalent to: value = value / 4;
+value %= 3; // Equivalent to: value = value % 3;
+
+console.log("\nAssignment Operators:");
+console.log("Value:", value);
+
+// Comparison Operators
+let a = 10;
+let b = 5;
+let isEqual = a == b;
+let isNotEqual = a != b;
+let isGreaterThan = a > b;
+let isLessThan = a < b;
+let isGreaterOrEqual = a >= b;
+let isLessOrEqual = a <= b;
+
+console.log("\nComparison Operators:");
+console.log("Is Equal:", isEqual);
+console.log("Is Not Equal:", isNotEqual);
+console.log("Is Greater Than:", isGreaterThan);
+console.log("Is Less Than:", isLessThan);
+console.log("Is Greater or Equal:", isGreaterOrEqual);
+console.log("Is Less or Equal:", isLessOrEqual);
+
+// Logical Operators
+let p = true;
+let q = false;
+let andOperator = p && q;
+let orOperator = p || q;
+let notOperator = !p;
+
+console.log("\nLogical Operators:");
+console.log("AND Operator:", andOperator);
+console.log("OR Operator:", orOperator);
+console.log("NOT Operator:", notOperator);
