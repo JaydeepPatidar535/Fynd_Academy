f2d1357f45f792c68e904c619eb4b0e3b2bbc8ce
JaydeepPatidar535 <jaydeeppatidar535@gmail.com>
2023-10-04 18:47:18 +0530

    New Files

---
+++ JavaScript/functions.js
@@ -0,0 +1,19 @@
+// Parameterized Arrow Function
+const add = (a, b) => {
+    return a + b;
+  };
+  
+  // Anonymous Function
+  const subtract = function(x, y) {
+    return x - y;
+  };
+  
+  // Using the functions
+  console.log(add(5, 3)); // Output: 8
+  console.log(subtract(10, 4)); // Output: 6
+  
+  // Examples (Fynd_class)
+  const display=()=>{
+    console.log('Hello');
+  }
+  console.log(display());
