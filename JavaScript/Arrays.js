467eb7abd048389996fa36fde2a590a6d2f3872b
JaydeepPatidar535 <jaydeeppatidar535@gmail.com>
2023-10-12 12:01:20 +0530

    New Files added

---
+++ JavaScript/Arrays.js
@@ -0,0 +1,20 @@
+// let data =[1,2,"jaydeep"];
+// console.log(data);
+// console.log(typeof(data))
+
+// Using new Keyword
+
+// let a = new Array(3,3,4,5,5,"Jaydeep");
+// console.log(a);
+// console.log(typeof(a));
+
+// Map method
+
+// let circle = [10,20,30];
+// let circlearea = circle.map((e)=>{
+//     return e*e*3.14;
+// });
+// console.log(circlearea)
+
+
+
---
+++ JavaScript/Strings.js
@@ -0,0 +1,5 @@
+// let s = 'Peter';
+// console.log(s);
+// console.log(typeof(s));
+
+let name3 = new String("Peter");
